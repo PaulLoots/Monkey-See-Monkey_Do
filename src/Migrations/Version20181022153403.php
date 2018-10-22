e34e2a6f37aab06b9ddd9f5f16dc5f67d992acc0
Paul Loots <170083@virtualwindow.co.za>
2018-10-22 18:15:03 +0200

    add answer functionality to the riddle page

---
+++ src/Migrations/Version20181022153403.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181022153403 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE answer ADD likes INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE answer ADD dislikes INT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE answer DROP likes');
+        $this->addSql('ALTER TABLE answer DROP dislikes');
+    }
+}
