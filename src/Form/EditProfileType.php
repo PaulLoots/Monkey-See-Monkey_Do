d9284cd3301faea56d1e4aa39d75ac8bcc07d74a
Armand Pretorius <m.armand.pretorius@gmail.com>
2018-10-22 22:49:18 +0200

    Added EditProfile Controller and form (error)

---
+++ src/Form/EditProfileType.php
@@ -0,0 +1,26 @@
+<?php
+//src/Form/EditProfileType.php
+namespace App\Form;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class EditProfileType extends AbstractType
+{
+    // public function buildForm(FormBuilderInterface $builder, array $options)
+    // {
+    //     $builder
+    //         //->add('image_path', FileType::class)
+    //         ->add('name', TextType::class)
+    //         ->add('email', EmailType::class)
+    //         ->add('password', PasswordType::class)
+    //         ->add('submitEditProfile', SubmitType::class, ['label' => 'Save'])
+    //     ;
+    // }
+}
+?>
