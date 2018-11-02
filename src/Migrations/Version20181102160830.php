777a7b2a7a8e5c91a0668b035b8a9361f70fa6d9
Armand Pretorius <m.armand.pretorius@gmail.com>
2018-11-02 18:23:15 +0200

    Add Report Functionality on MyRiddle Page

---
+++ src/Migrations/Version20181102160830.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181102160830 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE comment ADD is_gif BOOLEAN DEFAULT NULL');
+        $this->addSql('ALTER TABLE profile_image ADD image BYTEA DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE profile_image DROP image');
+        $this->addSql('ALTER TABLE comment DROP is_gif');
+    }
+}
